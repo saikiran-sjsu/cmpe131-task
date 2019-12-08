7edf240602c9afa48383d6552221ca44bfd4d936
Saikiran Vaidhyanathan <saikiran.vaidhyanathan@sjsu.edu>
2019-12-07 23:38:06 -0800

    ...

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,19 @@
 from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+
+
+login = LoginManager(app)
+# right side is the function that's called to login users
+login.login_view = 'login'
+
+
 
 from app import routes
 
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,8 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request
+from flask_login import login_required, current_user, login_user
+from app.forms import LoginForm, RegisterForm, TaskForm, ForgotForm
 from app import app
+
 
 @app.route('/')
 @app.route('/home')
@@ -7,3 +10,33 @@ def home():
     title = 'Home'
     return render_template('home.html', title=title)
 
+
+@app.route('/landing')
+@login_required
+def landing():
+    title = 'Landing Page'
+    return render_template('landing.html', title=title)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    title = 'Login | Task Organizer'
+
+    if current_user.is_authenticated:
+        return redirect(url_for('landing'))
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('login')
+
+        return redirect(next_page)
+
+    return render_template('login.html', title=title, form=form)
