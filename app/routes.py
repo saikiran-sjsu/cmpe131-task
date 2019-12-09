70b044af0680b47772cbe030c75765699312dc7c
Saikiran Vaidhyanathan <saikiran.vaidhyanathan@sjsu.edu>
2019-12-09 11:24:39 -0800

    added more to routes

--- app/routes.py
+++ app/routes.py
@@ -1,19 +1,33 @@
-from flask import render_template, redirect, url_for, request, flash
-from flask_login import login_required, current_user, login_user
-from werkzeug.urls import url_parse
-
-from app.forms import LoginForm, RegisterForm, TaskForm, ForgotForm
-from app import app, db
-
-from app.models import User, Task
-
+from flask import render_template, flash, redirect, url_for, Markup
+from TaskOrganizer.myapp import app
+from TaskOrganizer.myapp import db
+from TaskOrganizer.myapp.form import LoginForm, RegisterForm, TaskForm, ForgotForm
+from flask_login import current_user, login_user
+from flask_login import logout_user
+from flask_login import login_required
+from flask import request
+from TaskOrganizer.myapp.models import User, Task
 
 
 @app.route('/')
-@app.route('/home')
+@app.route('/home', methods=['GET', 'POST'])
 def home():
+
+    name = 'Sai'
     title = 'Home'
-    return render_template('home.html', title=title)
+    form = LoginForm()
+
+    return render_template('home.html', title=title, name=name, form=form)
+
+
+@app.route('/logout')
+def logout():
+    user = current_user
+    user.authenticated = False
+    db.session.add(user)
+    db.session.commit()
+    logout_user()
+    return redirect(url_for('home'))
 
 
 @app.route('/landing')
@@ -21,6 +35,26 @@ def home():
 def landing():
     title = 'Landing Page'
     return render_template('landing.html', title=title)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+
+    title = 'Register | Task Organizer'
+    form = RegisterForm()
+    if form.validate_on_submit():
+        users = User(username=form.username.data, email=form.email.data,
+                     first_name=form.first_name.data, last_name=form.last_name.data)
+        users.set_password(form.password.data)
+        users.set_answer(form.question.data)
+        db.session.add(users)
+        db.session.commit()
+
+        # message = Markup()
+        flash('Account Created!' + str(users.first_name))
+        # print("Account!")
+        return redirect(url_for('login'))
+    return render_template('register.html', title=title, form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -47,21 +81,98 @@ def login():
     return render_template('login.html', title=title, form=form)
 
 
-@app.route('/register', methods=['GET', 'POST'])
-def register():
+@app.route('/add', methods=['GET', 'POST'])
+def add():
 
-    title = 'Register | Task Organizer'
-    form = RegisterForm()
-    if form.validate_on_submit():
-        users = User(username=form.username.data, email=form.email.data,
-                     first_name=form.first_name.data, last_name=form.last_name.data)
-        users.set_password(form.password.data)
-        users.set_answer(form.question.data)
-        db.session.add(users)
+    title = 'Add | Task Organizer'
+
+    tasks = Task.query.all()
+    # task = TaskForm(request.form)
+    task = TaskForm()
+    print('hefasdfasf')
+    if task.validate_on_submit():
+
+        tasks = Task(task_name=request.form['task_name'],
+                     description=request.form['description'], user_id=current_user.id)
+        db.session.add(tasks)
         db.session.commit()
 
-        # message = Markup()
-        flash('Account Created!' + str(users.first_name))
-        # print("Account!")
+        return redirect(url_for('add'))
+
+    return render_template('add.html', title="add task", form=task, tasks=tasks)
+
+
+@app.route('/view')
+def view():
+    title = 'View | Task Organizer'
+    tasks = Task.query.filter_by(user_id=current_user.id)
+
+    organize = Task.query.filter_by(user_id=current_user.id).order_by(Task.task_name)
+
+    return render_template('view.html', title=title, tasks=tasks, organize=organize)
+
+
+@app.route('/remove', methods=['GET', 'POST'])
+def remove():
+    title = 'Remove | Task Organizer'
+    # tasks = Task.query.all()
+
+    tasks = Task.query.filter_by(user_id=current_user.id)
+    form = TaskForm()
+
+    if form.validate_on_submit():
+        tasks = Task.query.filter_by(task_name=request.form['task_name']).first()
+        db.session.delete(tasks)
+        db.session.commit()
+        return redirect(url_for('remove'))
+    return render_template('remove.html', title=title, form=form, tasks=tasks)
+
+
+@app.route('/delete_all', methods=['POST'])
+def delete_all():
+
+    title = 'Remove | Task Organizer'
+    Task.query.delete()
+    tasks = Task.query.all()
+    db.drop_all(tasks)
+    db.session.commit()
+    tasks = Task.query.all()
+    form = TaskForm()
+    # if request.form['Yes']:
+    #    print('Check here')
+#   #     flash('Here')
+#
+#        return redirect(url_for('remove'))
+    return render_template('remove.html', title=title, form=form, tasks=tasks)
+
+
+@app.route('/forgot', methods=['GET', 'POST'])
+def forgot_password():
+
+    title = 'Forgot Password | Task Organizer'
+
+    form = ForgotForm()
+    print('hiss')
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        print('foo')
+        if user.check_answer(form.question.data):
+            print('moo')
+            return redirect(url_for('reset'))
+
+    return render_template('forgot.html', title=title, form=form)
+
+
+@app.route('/reset', methods=['GET', 'POST'])
+def reset():
+    title = 'Reset Password | Task Organizer'
+    form = ForgotForm()
+    print('hi')
+    if form.validate_on_submit():
+        print('hello')
+        user = User.query.filter_by(username=form.username.data).first()
+        user.set_password(form.reset_password.data)
+        db.session.add(user)
+        db.session.commit()
         return redirect(url_for('login'))
-    return render_template('register.html', title=title, form=form)
+    return render_template('reset.html', title=title, form=form)
